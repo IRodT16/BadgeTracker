a4692eb0357d920fbf37195361963f2244219d88
IRodT16 <rodeghier.ian@gmail.com>
2022-10-15 23:30:30 -0400

    first commit

---
+++ Server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require('dotenv').config({ path: './.env' });
+
+app.use(express.json());
+
+mongoose.connect(
+  process.env.DB_URL,
+  {
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log('Connected to DB...');
+    } else {
+      console.log('DB connection went wrong');
+    }
+  }
+);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}...`);
+});
